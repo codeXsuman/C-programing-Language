6fd10ceba9c2b1a610bbb894be472a2d559b47d7
Suman Nandi <sumannandiq7@gmail.com>
2025-11-17 14:05:32 +0000

    patternPrinting

---
+++ PATTERN-PRINTING/solid-rectangle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+*****
+*****
+*****
+*/
+int main () {
+    // printf("*****\n*****\n*****");
+
+    int rows, cols;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns:");
+    scanf("%d", &cols);
+
+    for (int j=1; j <= rows; j++) {
+        for (int i = 1; i <= cols; i++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+return 0;
+}
