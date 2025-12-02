76c3b4c28b613f302675fefd40b1cf5a46dfbc07
Suman Nandi <sumannandiq7@gmail.com>
2025-12-02 19:14:00 +0000

    2ndDEC

---
+++ FUNCTIONS/05_FunctionPrototype.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main () {
+    void fun(); // Function prototype declaration
+    fun(); // Function call
+    return 0;
+}
+
+void fun() {
+    printf("Hello");
+
+}
---
+++ FUNCTIONS/06_Combination&Permutation.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    int r;
+    printf("Enter r: ");
+    scanf("%d", &r);
+    int nfact=1; // n!
+    int rfact=1; // r!
+    int nminusrfact=1; // (n-r)!
+    for (int i=1; i<=n; i++) {
+        nfact = nfact * i;
+    }
+    for (int i=1; i<=r; i++) {
+        rfact = rfact * i;
+    }
+    for (int i=1; i<=n-r; i++) {
+        nminusrfact = nminusrfact * i;
+    }
+
+    int nCr = nfact / (rfact * nminusrfact);
+    printf("nCr = %d\n", nCr);
+
+    return 0;
+}
